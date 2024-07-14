ce0a4f028f44da032c54fba5fc2a30b69ee20414
Abdur Rahim <abdurrahimctg10@gmail.com>
2024-07-14 17:45:35 +0600

    chart problem solved

--- includes/views/student-profile.php
+++ includes/views/student-profile.php
@@ -289,60 +289,6 @@ foreach ($data_in_period as $item) {
         </div>
     </form>
 
-
-    <canvas id="courseDurationChart" width="400" height="200"></canvas>
-
-
-    <script>
-        jQuery(document).ready(function($) {
-            const dates = <?php echo json_encode($dates); ?>;
-            const durations = <?php echo json_encode($durations); ?>;
-
-            function renderChart(dates, durations) {
-                const ctx = document.getElementById('courseDurationChart').getContext('2d');
-
-                const chartData = {
-                    labels: dates,
-                    datasets: [{
-                        label: 'Total Duration (minutes)',
-                        data: durations,
-                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        borderWidth: 1
-                    }]
-                };
-
-                if (window.courseDurationChart) {
-                    window.courseDurationChart.destroy();
-                }
-
-                window.courseDurationChart = new Chart(ctx, {
-                    type: 'bar',
-                    data: chartData,
-                    options: {
-                        scales: {
-                            y: {
-                                beginAtZero: true,
-                                title: {
-                                    display: true,
-                                    text: 'Total Duration (minutes)'
-                                }
-                            },
-                            x: {
-                                title: {
-                                    display: true,
-                                    text: 'Date'
-                                }
-                            }
-                        }
-                    }
-                });
-            }
-
-            renderChart(dates, durations);
-        });
-    </script>
-
     <div id="tutor-course-details-list" class="tutor-mb-48">
         <div class="tutor-fs-5 tutor-fw-medium tutor-color-black tutor-mb-24">
             <?php esc_html_e('Course', 'tutor-pro'); ?>
@@ -592,9 +538,63 @@ foreach ($data_in_period as $item) {
 </div>
 
 
-<div class="video-watch-time-chart">
 
-</div>
+
+    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+    <script>
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            title:{
+                text: "Total Duration"
+            },
+            axisX:{
+                valueFormatString: "DD MMM"
+            },
+            axisY: {
+                title: "Number of minutes",
+
+            },
+            data: [{
+                type: "line",
+                xValueFormatString: "DD MMM",
+                color: "#F08080",
+                dataPoints: [
+                    { x: new Date(2017, 0, 1), y: 610 },
+                    { x: new Date(2017, 0, 2), y: 680 },
+                    { x: new Date(2017, 0, 3), y: 690 },
+                    { x: new Date(2017, 0, 4), y: 700 },
+                    { x: new Date(2017, 0, 5), y: 710 },
+                    { x: new Date(2017, 0, 6), y: 658 },
+                    { x: new Date(2017, 0, 7), y: 734 },
+                    { x: new Date(2017, 0, 8), y: 963 },
+                    { x: new Date(2017, 0, 9), y: 847 },
+                    { x: new Date(2017, 0, 10), y: 853 },
+                    { x: new Date(2017, 0, 11), y: 869 },
+                    { x: new Date(2017, 0, 12), y: 943 },
+                    { x: new Date(2017, 0, 13), y: 970 },
+                    { x: new Date(2017, 0, 14), y: 869 },
+                    { x: new Date(2017, 0, 15), y: 890 },
+                    { x: new Date(2017, 0, 16), y: 930 },
+                    { x: new Date(2017, 0, 17), y: 1850 },
+                    { x: new Date(2017, 0, 18), y: 1905 },
+                    { x: new Date(2017, 0, 19), y: 1980 },
+                    { x: new Date(2017, 0, 20), y: 1858 },
+                    { x: new Date(2017, 0, 21), y: 1034 },
+                    { x: new Date(2017, 0, 22), y: 963 },
+                    { x: new Date(2017, 0, 23), y: 847 },
+                    { x: new Date(2017, 0, 24), y: 853 },
+                    { x: new Date(2017, 0, 25), y: 869 },
+                    { x: new Date(2017, 0, 26), y: 943 },
+                    { x: new Date(2017, 0, 27), y: 970 },
+                    { x: new Date(2017, 0, 28), y: 869 },
+                    { x: new Date(2017, 0, 29), y: 890 },
+                    { x: new Date(2017, 0, 30), y: 930 },
+                    { x: new Date(2017, 0, 31), y: 750 }
+                ]
+            }]
+        });
+        chart.render();
+    </script>
 
 <div id="tutor-report-reviews">
     <div class="tutor-fs-5 tutor-fw-medium tutor-color-black tutor-mb-24">

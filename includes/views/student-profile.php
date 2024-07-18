3e1671e1d23ccb04978fe948e39467c7d690edf7
ftanvir <ft.tanvir551@gmail.com>
2024-07-18 12:06:48 +0600

    json data updated

--- includes/views/student-profile.php
+++ includes/views/student-profile.php
@@ -82,15 +82,15 @@ function get_chart_data($period = 'today', $start_date = '', $end_date = '') {
     switch ($query_condition) {
 
         case 'days':
-            $query = "SELECT DATE(date) as period, SUM(total_watch_time)/3600 as duration 
+            $query = "SELECT DATE(date) as period, SUM(total_watch_time)/60 as duration 
                       FROM $table_name WHERE $date_query GROUP BY DATE(date)";
             break;
         case 'months':
-            $query = "SELECT DATE_FORMAT(date, '%Y-%m') as period, SUM(total_watch_time)/3600 as duration 
+            $query = "SELECT DATE_FORMAT(date, '%Y-%m') as period, SUM(total_watch_time)/60 as duration 
                       FROM $table_name WHERE $date_query GROUP BY DATE_FORMAT(date, '%Y-%m')";
             break;
         default:
-            $query = "SELECT YEAR(date) as period, SUM(total_watch_time)/3600 as duration 
+            $query = "SELECT YEAR(date) as period, SUM(total_watch_time)/60 as duration 
                       FROM $table_name WHERE $date_query GROUP BY YEAR(date)";
             break;
     }
@@ -174,6 +174,10 @@ $data_in_period = fetch_video_progress_by_parameters($period, $start_date, $end_
 
 
 $chart_data = get_chart_data($period, $start_date, $end_date);
+
+$chart_data_to_json = json_encode($chart_data);
+
+ray('json', $chart_data_to_json);
 ?>
 
 <div id="tutor-report-student-details" class="tutor-report-common">
@@ -632,6 +636,16 @@ $chart_data = get_chart_data($period, $start_date, $end_date);
 
     <div id="chartContainer" style="height: 370px; width: 100%;"></div>
     <script>
+        var dataPoints = <?php echo $chart_data_to_json; ?>;
+        console.log(dataPoints);
+
+        dataPoints = dataPoints.map(function (data) {
+            return {
+                x: new Date(data.period),
+                y: data.duration
+            };
+        });
+        console.log(dataPoints);
         var chart = new CanvasJS.Chart("chartContainer", {
             animationEnabled: true,
             title:{
@@ -641,46 +655,14 @@ $chart_data = get_chart_data($period, $start_date, $end_date);
                 valueFormatString: "DD MMM"
             },
             axisY: {
-                title: "Number of minutes",
+                title: "Number of hours",
 
             },
             data: [{
                 type: "line",
                 xValueFormatString: "DD MMM",
                 color: "#F08080",
-                dataPoints: [
-                    { x: new Date(2017, 0, 1), y: 610 },
-                    { x: new Date(2017, 0, 2), y: 680 },
-                    { x: new Date(2017, 0, 3), y: 690 },
-                    { x: new Date(2017, 0, 4), y: 700 },
-                    { x: new Date(2017, 0, 5), y: 710 },
-                    { x: new Date(2017, 0, 6), y: 658 },
-                    { x: new Date(2017, 0, 7), y: 734 },
-                    { x: new Date(2017, 0, 8), y: 963 },
-                    { x: new Date(2017, 0, 9), y: 847 },
-                    { x: new Date(2017, 0, 10), y: 853 },
-                    { x: new Date(2017, 0, 11), y: 869 },
-                    { x: new Date(2017, 0, 12), y: 943 },
-                    { x: new Date(2017, 0, 13), y: 970 },
-                    { x: new Date(2017, 0, 14), y: 869 },
-                    { x: new Date(2017, 0, 15), y: 890 },
-                    { x: new Date(2017, 0, 16), y: 930 },
-                    { x: new Date(2017, 0, 17), y: 1850 },
-                    { x: new Date(2017, 0, 18), y: 1905 },
-                    { x: new Date(2017, 0, 19), y: 1980 },
-                    { x: new Date(2017, 0, 20), y: 1858 },
-                    { x: new Date(2017, 0, 21), y: 1034 },
-                    { x: new Date(2017, 0, 22), y: 963 },
-                    { x: new Date(2017, 0, 23), y: 847 },
-                    { x: new Date(2017, 0, 24), y: 853 },
-                    { x: new Date(2017, 0, 25), y: 869 },
-                    { x: new Date(2017, 0, 26), y: 943 },
-                    { x: new Date(2017, 0, 27), y: 970 },
-                    { x: new Date(2017, 0, 28), y: 869 },
-                    { x: new Date(2017, 0, 29), y: 890 },
-                    { x: new Date(2017, 0, 30), y: 930 },
-                    { x: new Date(2017, 0, 31), y: 750 }
-                ]
+                dataPoints: dataPoints
             }]
         });
         chart.render();
